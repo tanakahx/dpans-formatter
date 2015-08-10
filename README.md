dcf981f3997feeb50517ffdac34dd7f3fdf36336
Hiroyuki Tanaka <tanakahx@gmail.com>
2015-08-10 23:41:32 +0900

    Push source code and README.md

---
+++ README.md
@@ -0,0 +1,92 @@
+# dpans-formatter
+Script for formatting draft proposed American National Standard (dpANS) on Common Lisp so that each section has its own contents in place.
+
+## Environment
+
+* SBCL 1.2.14
+* Quicklisp 2015-01-28
+
+## Usage
+
+### Step 1. Clone this repository
+
+```console
+$ git clone https://github.com/tanakahx/dpans-formatter.git
+$ cd dpans-formatter
+```
+
+### Step 2. Download dpans2texi.el to make dpANS HTML document
+
+[dpans2texi.el](http://users-phys.au.dk/harder/dpans.html) is necesarry for converting dpANS TeX source to HTML document. First of all, download and configure it.
+
+```console
+$ wget http://users-phys.au.dk/harder/dpans2texi-1.05.tar.gz
+$ tar zxvf dpans2texi-1.05.tar.gz
+$ cd dpans2texi-1.05
+$ ./configure
+```
+
+Run the following command to download TeX sources and convert texinfo and HTML format.
+
+```
+$ make wget
+$ make
+$ make html
+```
+
+`ansicl.html` is created and includes converted HTML documents.
+
+### Step 3. Run dpans-formatter
+
+dpans-formatter can be run from your shell directly. The argument is the *index* file of the HTML document, namely `index.html`. In case of Windows and Mac OS X, however, they does not have case-sensitivity on filename, converted `index.html` and `Index.html` is identified as the same file, so you have to *escape* `index.html` to a safe place by renaming it `ansicl.html` for example.
+
+```console
+$ mv ansicl.html/index.html ansicl.html/ansicl.html # prevent Windows and Mac OS X from identifying with Index.html
+```
+
+When you run the script, you get the following outputs.
+
+```console
+$ ./dpans-formatter.lisp ansicl.html/ansicl.html
+Making a directory ansicl.html.formatted/
+Copying files from ansicl.html/ to ansicl.html.formatted/
+Converting Ch. Credits                    => ansicl.html.formatted/Credits.html                     (  42158 bytes)
+Converting Ch. Introduction               => ansicl.html.formatted/Introduction.html                ( 142530 bytes)
+Converting Ch. Syntax                     => ansicl.html.formatted/Syntax.html                      ( 138579 bytes)
+Converting Ch. Evaluation-and-Compilation => ansicl.html.formatted/Evaluation-and-Compilation.html  ( 174750 bytes)
+Converting Ch. Types-and-Classes          => ansicl.html.formatted/Types-and-Classes.html           (  69293 bytes)
+Converting Ch. Data-and-Control-Flow      => ansicl.html.formatted/Data-and-Control-Flow.html       (  42560 bytes)
+Converting Ch. Iteration                  => ansicl.html.formatted/Iteration.html                   (  79692 bytes)
+Converting Ch. Objects                    => ansicl.html.formatted/Objects.html                     (  90823 bytes)
+Converting Ch. Structures                 => ansicl.html.formatted/Structures.html                  (   2273 bytes)
+Converting Ch. Conditions                 => ansicl.html.formatted/Conditions.html                  (  37083 bytes)
+Converting Ch. Symbols                    => ansicl.html.formatted/Symbols.html                     (   4742 bytes)
+Converting Ch. Packages                   => ansicl.html.formatted/Packages.html                    (  29034 bytes)
+Converting Ch. Numbers                    => ansicl.html.formatted/Numbers.html                     (  43384 bytes)
+Converting Ch. Characters                 => ansicl.html.formatted/Characters.html                  (  23620 bytes)
+Converting Ch. Conses                     => ansicl.html.formatted/Conses.html                      (  15267 bytes)
+Converting Ch. Arrays                     => ansicl.html.formatted/Arrays.html                      (  16962 bytes)
+Converting Ch. Strings                    => ansicl.html.formatted/Strings.html                     (   4732 bytes)
+Converting Ch. Sequences                  => ansicl.html.formatted/Sequences.html                   (  16573 bytes)
+Converting Ch. Hash-Tables                => ansicl.html.formatted/Hash-Tables.html                 (  11227 bytes)
+Converting Ch. Filenames                  => ansicl.html.formatted/Filenames.html                   (  40443 bytes)
+Converting Ch. Files                      => ansicl.html.formatted/Files.html                       (  10518 bytes)
+Converting Ch. Streams                    => ansicl.html.formatted/Streams.html                     (  27257 bytes)
+Converting Ch. Printer                    => ansicl.html.formatted/Printer.html                     ( 142673 bytes)
+Converting Ch. Reader                     => ansicl.html.formatted/Reader.html                      (  11961 bytes)
+Converting Ch. System-Construction        => ansicl.html.formatted/System-Construction.html         (  11094 bytes)
+Converting Ch. Environment                => ansicl.html.formatted/Environment.html                 (  16219 bytes)
+Converting Ch. Glossary                   => ansicl.html.formatted/Glossary.html                    ( 239642 bytes)
+Converting Ch. Appendix                   => ansicl.html.formatted/Appendix.html                    (   5736 bytes)
+Converting Ch. Indecies                   => ansicl.html.formatted/Indecies.html                    ( 340110 bytes)
+Converting Ch. Symbol-Index               => ansicl.html.formatted/Symbol-Index.html                ( 252143 bytes)
+Converting Ch. List-of-Figures            => ansicl.html.formatted/List-of-Figures.html             (  20597 bytes)
+Converting Ch. Table-of-Contents          => ansicl.html.formatted/Table-of-Contents.html           (  88091 bytes)
+$
+```
+
+All formatted outputs are saved in `ansicl.html.formatted` directory in this example. The index of the document directory is now `ansicl.html.formatted/index.html` and we can view them with web browser.
+
+```console
+$ open ansicl.html.formatted/index.html
+```
---
+++ dpans-formatter.lisp
@@ -0,0 +1,157 @@
+#|
+exec /usr/local/bin/sbcl --noinform --non-interactive --load "$0" "$@"
+|#
+
+;;
+;; Script for formatting draft proposed American National Standard (dpANS) on Common Lisp
+;; so that each section has its own contents in place
+;;
+;; Usage: sbcl dpans-formatter.lisp DPANS_INDEX_HTML
+;;
+;;        DPANS_DIRECTORY is a top directory of Dpans.
+;;
+
+(in-package :cl-user)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload :alexandria :silent t)
+  (ql:quickload :cl-fad :silent t)
+  (ql:quickload :cl-ppcre :silent t))
+
+(defpackage dpans
+  (:use cl)
+  (:import-from :alexandria
+                :iota
+                :read-file-into-string
+                :write-string-into-file)
+  (:import-from :cl-ppcre
+                :scan
+                :register-groups-bind
+                :regex-replace
+                :regex-replace-all)
+  (:import-from :cl-fad
+                :pathname-as-directory
+                :pathname-directory-pathname
+                :merge-pathnames-as-file
+                :list-directory))
+
+(in-package :dpans)
+
+(defparameter *src-path* nil)   ; Source directory including original dpANS HTML documents
+(defparameter *dst-path* nil)   ; Formatted dpANS HTML documents are saved in this directory
+(defparameter *index-name* nil) ; Top HTML filename (e.g. index.html)
+
+(defun scan-href (tag)
+  "Pick up an href value of <A> tag."
+  (register-groups-bind (tag)
+      ("href=\"([^\"#]*)#.*\"" tag)
+    tag))
+
+(defun iterate-on (tag menu scanner)
+  "Iterate on each tag"
+  (labels ((rec (acc &key (start 0))
+             (multiple-value-bind (begin end)
+                 (scan tag menu :start start)
+               (if (and begin end)
+                   (rec (cons (subseq menu begin (length menu)) acc)
+                        :start end)
+                   (mapcar scanner (reverse acc))))))
+    (rec nil)))
+
+(defun body-content (src)
+  "Return the contents of <body> in src"
+  (register-groups-bind (content)
+      ("(?s)<div class=\"node\">.*?</div>(.*)</body>" src)
+    content))
+
+(defun link-content (link)
+  "Return the contents of a given link"
+  (let* ((path (merge-pathnames-as-file *src-path* link)))
+    (body-content (read-file-into-string path))))
+
+(defun scan-menu-list (src)
+  "Search a <ul> tag which is a menu list except for a dictionary list."
+  (multiple-value-bind (begin end)
+      (scan "(?s)<ul class=\"menu\">.*?</ul>" src)
+    (let ((dict (scan "(?m)^<p>Dictionary$" src)))
+      (if (or (null dict)
+              (and dict (< begin dict)))
+          (values begin end)))))
+
+(defun replace-menu (src)
+  "Replace menu part in src with its contents in place recursively."
+  (multiple-value-bind (begin end)
+      (scan-menu-list src)
+    (if (and begin end)
+        (let ((menu (subseq src begin end))
+              (head (subseq src 0 begin))
+              (tail (subseq src end (length src))))
+          (apply #'concatenate 
+                 (append (list 'string head "<hr>")
+                         (mapcar #'replace-menu
+                                 (mapcar #'link-content (Iterate-on "<li>" menu #'scan-href)))
+                         (list tail))))
+        ;; a leaf document
+        (concatenate 'string src "<hr>"))))
+
+(defun file-size (path)
+  "Return file size in byte"
+  (with-open-file (in path)
+    (file-length in)))
+
+(defun set-params (index-path)
+  "Set up global parameters."
+  (setf *src-path* (pathname-directory-pathname index-path))
+  (setf *dst-path* (pathname-as-directory
+                    (concatenate 'string
+                                 (subseq index-path 0 (1- (length (namestring *src-path*))))
+                                 ".formatted")))
+  (setf *index-name* (file-namestring index-path)))
+
+(defun copy-all-files ()
+  "Copy all files from *src-path* to *dst-path*.
+A file named 'Index.html' and references to it are renamed to 'Indecies.html',
+because this name is conflicting with 'index.html'. Besides, *index-name* is also renamed to 'index.html'.
+This can be safely done, becase 'Index.html' is renamed to another name beforehand and no conflics are occured. "
+  (flet ((replace-link-to-index (src)
+           (regex-replace-all "([/\"])Index\\.html" src "\\1Indecies.html")))
+    (format t "Making a directory ~a~%" *dst-path*)
+    (ensure-directories-exist *dst-path*)
+    (format t "Copying files from ~a to ~a~%" *src-path* *dst-path*)
+    (dolist (i (list-directory *src-path*))
+      (let ((out-path (cond ((scan "/Index\\.html$" (namestring i))
+                             (merge-pathnames-as-file *dst-path* "Indecies.html"))
+                            ((scan (format nil "~a$" *index-name*) (namestring i))
+                             (merge-pathnames-as-file *dst-path* "index.html"))
+                            (t
+                             (merge-pathnames-as-file *dst-path* (file-namestring i)))))
+            (src (replace-link-to-index (read-file-into-string i))))
+        (write-string-into-file src out-path :if-exists :supersede)))))
+
+(defun flatten-sections-on-chapters ()
+  "Flatten sections which has an <A> tag on each chapter document."
+  (let* ((in-path (merge-pathnames-as-file *dst-path* "index.html"))
+         (index (read-file-into-string in-path)))
+    (multiple-value-bind (begin end)
+        (scan-menu-list index)
+      (if (and begin end)
+          (let* ((menu (subseq index begin end))
+                 (chapter-list (iterate-on "<li>" menu #'scan-href)))
+            (dolist (c chapter-list)
+              (let ((in-path (merge-pathnames-as-file *dst-path* c))
+                    (out-path (merge-pathnames-as-file *dst-path* c)))
+                (write-string-into-file (replace-menu (read-file-into-string in-path))
+                                        out-path :if-exists :supersede)
+                (format t "Converting Ch. ~27a=> ~54a (~7d bytes)~%"
+                        (regex-replace "\.html" c "") out-path (file-size out-path)))))))))
+
+(defun main (args)
+  "args must include pathname of an index HTML file."
+  (when (< (length args) 1)
+    (format t "USAGE: dpANS index HTML is required as an argument.~%")
+    (return-from main))
+  (set-params (car args))
+  (copy-all-files)
+  (flatten-sections-on-chapters))
+
+(main uiop:*command-line-arguments*)
